b6c0b85b39c6404714cf21c4e5501cce4fb60e97
Stella Goodrich <goodrist@mail.gvsu.edu>
2025-10-16 20:12:03 -0400

    set up basic server and beginning of frontend

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname)); // Serve frontend files
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
